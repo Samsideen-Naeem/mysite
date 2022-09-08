8b9ad01d47517d4c09b43dca8a91f28ccc138ae7
Samsideen-Naeem <110115318+Samsideen-Naeem@users.noreply.github.com>
2022-09-08 15:56:03 +0000

    added validation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import Home from './pages/Home'
-import {Routes, Route} from "react-router-dom"
+import React, { useState } from 'react'
+import ProfileCard from './components/ProfileCard'
+import ProfileForm  from './components/ProfileForm'
 
 function App() {
+
+
+  const [allProfile, setAllprofiles] = useState([
+
+    {
+      firstname:"Samsideen",
+      lastname: "Naeem",
+      email:"comfort@gmail.com",
+      phone:"0246183596"
+    }
+  ])
+
+const updateprofile = (profile) => {
+  let arr = allProfile;
+  arr.push(profile)
+  setAllprofiles([...arr])
+};
+
   return (
-    <Routes>
-   <Route path="/" element={<Home/>} />
-    
-    
-    </Routes>
+    <>
+    <div className='app'>
+  <h1>Our profile maker</h1>
+    </div>
+    <ProfileForm submit={updateprofile} /> 
+    <hr />
+    <div className='list'>
+      {allProfile.map((person, index) =>(
+       <ProfileCard key={index} card={person} /> 
+      ))}
+
+
+
+    </div>
+    </>
   )
 }
 
